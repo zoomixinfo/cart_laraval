c032647fcd1f71a338577cfb0f078c1b66b3736b
Sameer Kongath <80168263+zoomixinfo@users.noreply.github.com>
2021-09-19 16:12:10 +0530

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel Add To Cart Function - ItSolutionStuff.com</title>
+    <title>Laravel Add To Cart Function</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
